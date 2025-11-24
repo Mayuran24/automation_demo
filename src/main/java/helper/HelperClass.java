19106e0b3a787ac7323c4133f307b765e1f8e6a1
Mayuran <mayuran@planyear.com>
2025-11-24 12:04:39 +0530

    Updated Logger library

--- src/main/java/helper/HelperClass.java
+++ src/main/java/helper/HelperClass.java
@@ -1,18 +1,19 @@
 package helper;
 
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 public class HelperClass {
     private WebDriverWait wait;
     protected WebDriver driver;
-    private static final Logger logger = org.apache.logging.log4j.LogManager.getLogger(HelperClass.class);
+    private static final Logger logger = LoggerFactory.getLogger(HelperClass.class);
 
     public HelperClass(WebDriver driver){
         this.driver = driver;
